f23b6c22edd5709b83a4dd870e977f8d51d56e1e
Iaroslav Morozov <teun@tuta.io>
2025-03-30 01:26:10 +0200

    Add the `ISystemSoftwareRepository` interface

---
+++ Teulog.Core/Repositories/ISystemSoftwareRepository.fs
@@ -0,0 +1,11 @@
+﻿namespace Teulog.Core.Repositories
+
+open System
+open Teulog.Core.Entities
+
+type ISystemSoftwareRepository =
+    abstract Add: SystemSoftware -> unit
+    abstract GetById: Guid -> SystemSoftware
+    abstract GetAll: unit -> SystemSoftware list
+    abstract Update: SystemSoftware -> unit
+    abstract Delete: SystemSoftware -> unit
