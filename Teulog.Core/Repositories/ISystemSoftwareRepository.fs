55f27762a2295229d1efbfc2a8e806564cd24be7
Iaroslav Morozov <teun@tuta.io>
2025-03-30 02:00:30 +0200

    Change the return type of the `GetById` method to `SystemSoftware option`

--- Teulog.Core/Repositories/ISystemSoftwareRepository.fs
+++ Teulog.Core/Repositories/ISystemSoftwareRepository.fs
@@ -5,7 +5,7 @@ open Teulog.Core.Entities
 
 type ISystemSoftwareRepository =
     abstract Add: SystemSoftware -> unit
-    abstract GetById: Guid -> SystemSoftware
+    abstract GetById: Guid -> SystemSoftware option
     abstract GetAll: unit -> SystemSoftware list
     abstract Update: SystemSoftware -> unit
     abstract Delete: SystemSoftware -> unit
