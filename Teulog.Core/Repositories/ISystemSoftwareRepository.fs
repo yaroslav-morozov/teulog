0cf4ae656e6241ddc7c53cb58d70234a5396eba1
Iaroslav Morozov <teun@tuta.io>
2025-03-30 02:07:16 +0200

    Refactor `ISystemSoftwareRepository` for async support

--- Teulog.Core/Repositories/ISystemSoftwareRepository.fs
+++ Teulog.Core/Repositories/ISystemSoftwareRepository.fs
@@ -4,8 +4,8 @@ open System
 open Teulog.Core.Entities
 
 type ISystemSoftwareRepository =
-    abstract Add: SystemSoftware -> unit
-    abstract GetById: Guid -> SystemSoftware option
-    abstract GetAll: unit -> SystemSoftware list
-    abstract Update: SystemSoftware -> unit
-    abstract Delete: SystemSoftware -> unit
+    abstract AddAsync: SystemSoftware -> Async<unit>
+    abstract GetByIdAsync: Guid -> Async<SystemSoftware option>
+    abstract GetAllAsync: unit -> Async<SystemSoftware list>
+    abstract UpdateAsync: SystemSoftware -> Async<unit>
+    abstract DeleteAsync: SystemSoftware -> Async<unit>
